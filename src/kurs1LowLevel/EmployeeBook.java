6797c122db97bc7e021958703323855bafe99957
Yurii <shadowjura82@gmail.com>
2023-04-29 01:56:32 +0200

    Middle level has been completed

--- src/kurs1LowLevel/EmployeeBook.java
+++ src/kurs1LowLevel/EmployeeBook.java
@@ -17,6 +17,14 @@ public class EmployeeBook {
         return result;
     }
 
+    public String toString(int departmentID) {
+        String result = "";
+        for (Employee emp : employees)
+            if (emp.getDepartment() == departmentID)
+                result += "Ф.И.О.: " + emp.getFullName() + " Зарплата: " + emp.getSalary() + "\n";
+        return result;
+    }
+
     public int salarySumm() {
         int summ = 0;
         for (Employee emp : employees) summ += emp.getSalary();
@@ -46,5 +54,66 @@ public class EmployeeBook {
         for (Employee emp : employees)
             result += emp.getFullName() + "\n";
         return result;
+    }
+
+    public void indexingOfSalary(int percent) {
+        if (percent < 0) {
+            percent = Math.abs(percent);
+            for (Employee emp : employees)
+                //тут может быть не точность в расчетах т.к. нужно приводить к типу float, но т.к. по
+                // требованиям зарплата должна быть int, я не делал приведение типов
+                emp.setSalary(emp.getSalary() - (emp.getSalary() / 100 * percent));
+        } else {
+            percent = Math.abs(percent);
+            for (Employee emp : employees)
+                emp.setSalary(emp.getSalary() + (emp.getSalary() / 100 * percent));
+        }
+    }
+
+    public void indexingOfSalary(int percent, int departmentID) {
+        if (percent < 0) {
+            percent = Math.abs(percent);
+            for (Employee emp : employees)
+                //тут может быть не точность в расчетах т.к. нужно приводить к типу float, но т.к. по
+                // требованиям зарплата должна быть int, я не делал приведение типов
+                if (emp.getDepartment() == departmentID)
+                    emp.setSalary(emp.getSalary() - (emp.getSalary() / 100 * percent));
+        } else {
+            percent = Math.abs(percent);
+            for (Employee emp : employees)
+                if (emp.getDepartment() == departmentID)
+                    emp.setSalary(emp.getSalary() + (emp.getSalary() / 100 * percent));
+        }
+    }
+
+    public void departmentMethods(int departmentID) {
+        int count = 0;
+        int index = 0;
+        for (Employee emp : employees) if (emp.getDepartment() == departmentID) count++;
+        Employee[] bufEmpMass = new Employee[count];
+        for (Employee employee : employees)
+            if (employee.getDepartment() == departmentID) {
+                bufEmpMass[index] = employee;
+                index++;
+            }
+        EmployeeBook bufEmpBook = new EmployeeBook(bufEmpMass);
+        System.out.print("Сотрудник с минимальной зарплатой: " + bufEmpBook.minSalaryEmployee().toString() + "\n");
+        System.out.print("Сотрудник с максимальной зарплатой: " + bufEmpBook.maxSalaryEmployee().toString() + "\n");
+        System.out.print("Суммарная зарплата за месяц: " + bufEmpBook.salarySumm() + "\n");
+        System.out.printf("Средняя зарплата всех сотрудников отдела: %.2f\n", bufEmpBook.averageSalary());
+        indexingOfSalary(350, 1);
+        System.out.println("Список всех сотрудников: \n" + this.toString());
+        System.out.println("Список всех сотрудников отдела 1: \n" + this.toString(1));
+    }
+
+    public void salaryAnalyzer(int number) {
+        System.out.println("Cотрудники с зарплатой меньше числа:");
+        for (Employee emp : employees)
+            if (emp.getSalary() < number)
+                System.out.println("ID: " + emp.getId() + " Ф.И.О.: " + emp.getFullName() + " Зарплата: " + emp.getSalary());
+        System.out.println("Cотрудники с зарплатой больше (или равно) числа:");
+        for (Employee emp : employees)
+            if (emp.getSalary() >= number)
+                System.out.println("ID: " + emp.getId() + " Ф.И.О.: " + emp.getFullName() + " Зарплата: " + emp.getSalary());
     }
 }
--- src/kurs1LowLevel/Main.java
+++ src/kurs1LowLevel/Main.java
@@ -7,12 +7,21 @@ public class Main {
         Employee[] employees = new Employee[10];
         EmployeeBook employeeBook = new EmployeeBook(employees);
         fillMass(employees);
+
+        //Задачи базовой сложности
         System.out.println("Список всех сотрудников: \n" + employeeBook.toString());
         System.out.print("Суммарная зарплата за месяц: " + employeeBook.salarySumm() + "\n\n");
         System.out.print("Сотрудник с минимальной зарплатой: " + employeeBook.minSalaryEmployee().toString() + "\n\n");
         System.out.print("Сотрудник с максимальной зарплатой: " + employeeBook.maxSalaryEmployee().toString() + "\n\n");
         System.out.printf("Средняя зарплата всех сотрудников: %.2f\n\n", employeeBook.averageSalary());
         System.out.println("Ф.И.О. всех сотрудников: \n" + employeeBook.getFullName());
+
+        //Задачи повышенной сложности
+        employeeBook.indexingOfSalary(-45);
+        System.out.println("Список всех сотрудников: \n" + employeeBook.toString() + "\n\n");
+        System.out.println("Обработка сотрудников 1-го отдела: ");
+        employeeBook.departmentMethods(1);
+        employeeBook.salaryAnalyzer(1000);
     }
 
     public static void fillMass(Employee[] employees) {
